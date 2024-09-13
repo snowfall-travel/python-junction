09b1208e5814c1095fc0d70747f9dbd93f2957bc
Sam Bull <git@sambull.org>
2024-09-13 14:42:06 +0100

    Update __init__.py

--- junction/__init__.py
+++ junction/__init__.py
@@ -1,4 +1,8 @@
 from junction.client import JunctionClient as JunctionClient
-from junction.typedefs import Passenger as Passenger
+from junction.typedefs import (
+    Passenger as Passenger,
+    PlaceId as PlaceId,
+    PlaceType as PlaceType,
+)
 
 __version__ = "0.1"
