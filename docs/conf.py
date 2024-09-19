27f40f23a65bb406c596610d33b7e7d94412cdb2
Sam Bull <git@sambull.org>
2024-09-19 16:14:04 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,15 +8,13 @@
 import os
 import sys
 import re
+from pathlib import Path
 
-# Add the path to your SDK source code
-sys.path.insert(0, os.path.abspath('../../junction/'))
+PROJECT_ROOT_DIR = Path(__file__).parent.parent.resolve()
+VERSION_FILE_PATH = PROJECT_ROOT_DIR / "junction" / "__init__.py"
 
-# Get path for the file containing the version info
-init_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
-                              '../../junction/__init__.py')
 def get_version():
-    with open(init_file_path, 'r') as version_file:
+    with VERSION_FILE_PATH.open() as version_file:
         # Construct a regex pattern for the version variable passed
         pattern = re.compile(r'__version__\s*=\s*[\'"]([^\'"]+)[\'"]')
         for line in version_file:
@@ -28,21 +26,21 @@ def get_version():
 # Get the version
 version = get_version()
 
-project = 'Junction API SDK'
-copyright = '2024, SnowfallTravel'
-author = 'SnowfallTravel'
+project = "Junction API SDK"
+copyright = "2024, SnowfallTravel"
+author = "SnowfallTravel"
 release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'autoapi.extension',
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
 ]
-autoapi_dirs = ['../../junction/']  # Specify the directory where your SDK source code is located
+autoapi_dirs = ["../junction/"]  # Specify the directory where your SDK source code is located
 autoapi_generate_api_docs = True
-templates_path = ['_templates']
+templates_path = ["_templates"]
 # autoapi_ignore = ["*conf.py", "*booking.py", "*submodule.py", "*typedefs.py"]
 autoapi_ignore = ["*conf.py", "*typedefs.py"]
 
@@ -57,11 +55,11 @@ autoapi_options = [
 
 # Custom autodoc options
 autodoc_default_options = {
-    'members': True,                       # Include public members
-    'undoc-members': False,                # Do not include undocumented members
-    'private-members': False,              # Exclude members starting with '_'
-    'special-members': False,              # Exclude special members (__init__, __aenter__, etc.)
-    'exclude-members': '',  # Exclude specific members
+    "members": True,
+    "undoc-members": False,
+    "private-members": False,
+    "special-members": False,
+    "exclude-members": "",
 }
 
 # Enable autosummary (optional, useful if you want function/class summaries)
@@ -74,5 +72,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
