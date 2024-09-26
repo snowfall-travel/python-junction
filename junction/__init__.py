ff7d14b268b99842a49bb7ff473ab152d2ee14c6
Sam Bull <git@sambull.org>
2024-09-26 17:47:09 +0100

    Update __init__.py

--- junction/__init__.py
+++ junction/__init__.py
@@ -1,4 +1,8 @@
-from junction.client import JunctionClient as JunctionClient
+from junction.client import (
+    PROD as PROD,
+    SANDBOX as SANDBOX,
+    JunctionClient as JunctionClient,
+)
 from junction.typedefs import (
     Passenger as Passenger,
     PlaceId as PlaceId,
