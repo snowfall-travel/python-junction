5e290f3d6be2a531b33a10e5543c074aa59ad7fe
Sam Bull <git@sambull.org>
2024-09-26 15:24:38 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ from typing import AsyncIterator
 
 import pytest
 
+from junction import JunctionClient
+
 @pytest.fixture
 async def client() -> AsyncIterator[JunctionClient]:
     async with JunctionClient(os.environ["JUNCTION_API_KEY"]) as client:
